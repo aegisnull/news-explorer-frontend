2f7df70ef7a5de1ffc7bc07f64b5dcf444997563
Luis Tellez <luis@luistellez.com>
2022-12-14 20:37:19 -0800

    style: set logo in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from "../../images/logo.svg";
 
 function Header() {
   return (
     <header>
       <div className="header__logo">
-        <img src="#" alt="#" />
+        <img src={Logo} alt="NewsExplorer Logo" />
       </div>
       <nav className="header__nav">
         <ul className="header__nav-list">
