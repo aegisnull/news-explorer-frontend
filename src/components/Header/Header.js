62ffd8382a282344998c7c87e5bb47d2a96088fa
Luis Tellez <luis@luistellez.com>
2022-12-14 20:34:49 -0800

    feat: init Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="header__logo">
+        <img src="#" alt="#" />
+      </div>
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <li className="header__nav-item">Inicio</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
