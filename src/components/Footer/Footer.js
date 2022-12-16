106a0f212a4d78e42db5f798a877b4eb6fdf6803
Luis Tellez <luis@luistellez.com>
2022-12-15 19:25:47 -0800

    style: add Github and Facebook Logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Footer.scss";
+import GithubLogo from "../../images/github__logo.svg";
+import FacebookLogo from "../../images/facebook__logo.svg";
 
 function Footer() {
   return (
@@ -9,8 +11,22 @@ function Footer() {
           © {new Date().getFullYear()} Supersite, Powered by News API
         </p>
         <nav className="footer__nav">
-          <div className="footer__link">Inicio</div>
-          <div className="footer__link">Practicum</div>
+          <div className="footer__links">
+            <div className="footer__link">Inicio</div>
+            <div className="footer__link">Practicum</div>
+          </div>
+          <div className="footer__socials">
+            <img
+              className="footer__social"
+              src={GithubLogo}
+              alt="Github Logo"
+            />
+            <img
+              className="footer__social"
+              src={FacebookLogo}
+              alt="Facebook Logo"
+            />
+          </div>
         </nav>
       </div>
     </footer>
