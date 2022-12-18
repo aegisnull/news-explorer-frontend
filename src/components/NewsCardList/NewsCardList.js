cbb820e87eee514e7c336532cae152fd067d6c53
Luis Tellez <luis@luistellez.com>
2022-12-18 14:45:51 -0800

    feat: add Preloader to NewsCardList

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./NewsCardList.scss";
+import Preloader from "../Preloader/Preloader";
 
 function NewsCardList() {
-  return <section className="news-card-list"></section>;
+  return (
+    <section className="news-card-list">
+      <Preloader />
+    </section>
+  );
 }
 
 export default NewsCardList;
