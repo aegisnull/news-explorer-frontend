96d9cdd4e7034157ef9cfbe6e019081c26dcbf00
Luis Tellez <luis@luistellez.com>
2022-12-18 13:46:35 -0800

    fix: reset App.js for easy merge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Main from "./components/Main/Main";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
-import SavedNewsHeader from "./components/SavedNewsHeader/SavedNewsHeader";
 
 function App() {
   return (
     <div className="App">
-      <SavedNewsHeader />
-      {/* <Main />
-      <About /> */}
+      <Main />
+      <About />
       <Footer />
     </div>
   );
