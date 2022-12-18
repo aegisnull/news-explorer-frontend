74200ffff1c9db12b020e6a8786b81744c0edc69
Luis Tellez <luis@luistellez.com>
2022-12-18 13:56:21 -0800

    feat: Wrap components with Routes tag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Main from "./components/Main/Main";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Main />
-      <About />
-      <Footer />
+      <Routes>
+        <Main />
+        <About />
+        <Footer />
+      </Routes>
     </div>
   );
 }
