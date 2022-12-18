fa3b0574ebb6b25212df4ee5443728d4510252f2
Luis Tellez <luis@luistellez.com>
2022-12-18 13:24:45 -0800

    feat: finish HTML markup with hardcoded text for SavedNewsHeader component

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -6,6 +6,18 @@ function SavedNewsHeader() {
   return (
     <section className="saved-news-header">
       <Header />
+      <div className="saved-news-header__container">
+        <h2 className="saved-news-header__title">Artículos guardados</h2>
+        <p className="saved-news-header__subtitle">
+          Elise, tienes 5 artículos guardados
+        </p>
+        <p className="saved-news-header__keywords">
+          Palabras clave:{" "}
+          <span className="saved-news-header__keywords_bold">
+            Naturaleza, Yellowstone, y 2 más
+          </span>
+        </p>
+      </div>
     </section>
   );
 }
