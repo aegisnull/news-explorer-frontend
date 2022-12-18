55e6fc1d4e88372e30a39cdc12552e47ccd78cd7
Luis Tellez <luis@luistellez.com>
2022-12-18 13:21:15 -0800

    feat: init SavedNewsHeader component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Main from "./components/Main/Main";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
+import SavedNewsHeader from "./components/SavedNewsHeader/SavedNewsHeader";
 
 function App() {
   return (
     <div className="App">
-      <Main />
-      <About />
+      <SavedNewsHeader />
+      {/* <Main />
+      <About /> */}
       <Footer />
     </div>
   );
---
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SavedNewsHeader.scss";
+import Header from "../Header/Header";
+
+function SavedNewsHeader() {
+  return (
+    <section className="saved-news-header">
+      <Header />
+    </section>
+  );
+}
+
+export default SavedNewsHeader;
