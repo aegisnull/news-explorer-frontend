9f462d7e6ef8cdeb816f8838e3ecb8e6778a3e78
Luis Tellez <luis@luistellez.com>
2022-12-18 19:10:34 -0800

    feat: test card with hardcoded content

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./NewsCardList.scss";
 import Preloader from "../Preloader/Preloader";
+import NewsCard from "../NewsCard/NewsCard";
 
 function NewsCardList() {
   return (
     <section className="news-card-list">
       <Preloader />
+      <NewsCard />
     </section>
   );
 }
