2ddc058a38709278f35a795161adef4e405fcbb6
Luis Tellez <luis@luistellez.com>
2022-12-18 18:21:07 -0800

    feat: add light logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Logo from "../../images/logo.svg";
+import LightLogo from "../../images/logo_light.svg";
+
 import "./Header.scss";
 
 function Header() {
+  const currentPath = useLocation().pathname;
+  const logoPath = currentPath === "/saved-news" ? LightLogo : Logo;
+
   return (
     <header className="header">
       <div className="header__container">
         <div className="header__logo">
-          <img src={Logo} alt="NewsExplorer Logo" />
+          <img src={logoPath} alt="NewsExplorer Logo" />
         </div>
         <nav className="header__nav">
           <div className="header__link header__text">Inicio</div>
