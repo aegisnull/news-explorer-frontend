9aa99a8dda95bf6235230c1ee88a5a8be6008da3
Luis Tellez <luis@luistellez.com>
2022-12-18 20:58:01 -0800

    style: fix news card element positioning

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -7,6 +7,9 @@ function NewsCard() {
       <button className="card__save-button" />
       <button className="card__trash-button" />
       <button className="card__keyword-button"></button>
+      <button className="card__hover-text">
+        Inicia sesión para guardar artículos
+      </button>
 
       <a
         href="https://www.bbc.com/news/world-asia-64001554"
@@ -19,15 +22,17 @@ function NewsCard() {
           src="https://ichef.bbci.co.uk/news/1024/branded_news/0E62/production/_128028630_081078748-2.jpg"
           alt="Japan"
         />
-        <p className="card__date">December 15, 2022</p>
-        <h3 className="card__title">
-          Japan defence: China threat prompts plan to double military spending
-        </h3>
-        <p className="card__text">
-          It is the most dramatic change to Japan's security strategy since
-          adopting a pacifist constitution.
-        </p>
-        <p className="card__publisher">BBC News</p>
+        <div className="card__container">
+          <p className="card__date">December 15, 2022</p>
+          <h3 className="card__title">
+            Japan defence: China threat prompts plan to double military spending
+          </h3>
+          <p className="card__text">
+            It is the most dramatic change to Japan's security strategy since
+            adopting a pacifist constitution.
+          </p>
+          <p className="card__publisher">BBC News</p>
+        </div>
       </a>
     </article>
   );
