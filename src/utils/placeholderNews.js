bcfac8baaf7aa25bbf5b1fa1aa3b7cb8d1ca7129
Luis Tellez <luis@luistellez.com>
2022-12-18 21:43:34 -0800

    feat: create initialNews object to test before API implementation

---
+++ src/utils/placeholderNews.js
@@ -0,0 +1,64 @@
+const initialNews = [
+  {
+    source: "https://www.bbc.com/news/world-asia-64001554",
+    title:
+      "Japan defence: China threat prompts plan to double military spending",
+    publishedAt: "BBC News",
+    description:
+      "It is the most dramatic change to Japan's security strategy since adopting a pacifist constitution.",
+    urlToImage:
+      "https://ichef.bbci.co.uk/news/1024/branded_news/0E62/production/_128028630_081078748-2.jpg",
+  },
+  {
+    source: "https://www.bbc.com/news/world-asia-64001554",
+    title:
+      "Japan defence: China threat prompts plan to double military spending",
+    publishedAt: "BBC News",
+    description:
+      "It is the most dramatic change to Japan's security strategy since adopting a pacifist constitution.",
+    urlToImage:
+      "https://ichef.bbci.co.uk/news/1024/branded_news/0E62/production/_128028630_081078748-2.jpg",
+  },
+  {
+    source: "https://www.bbc.com/news/world-asia-64001554",
+    title:
+      "Japan defence: China threat prompts plan to double military spending",
+    publishedAt: "BBC News",
+    description:
+      "It is the most dramatic change to Japan's security strategy since adopting a pacifist constitution.",
+    urlToImage:
+      "https://ichef.bbci.co.uk/news/1024/branded_news/0E62/production/_128028630_081078748-2.jpg",
+  },
+  {
+    source: "https://www.bbc.com/news/world-asia-64001554",
+    title:
+      "Japan defence: China threat prompts plan to double military spending",
+    publishedAt: "BBC News",
+    description:
+      "It is the most dramatic change to Japan's security strategy since adopting a pacifist constitution.",
+    urlToImage:
+      "https://ichef.bbci.co.uk/news/1024/branded_news/0E62/production/_128028630_081078748-2.jpg",
+  },
+  {
+    source: "https://www.bbc.com/news/world-asia-64001554",
+    title:
+      "Japan defence: China threat prompts plan to double military spending",
+    publishedAt: "BBC News",
+    description:
+      "It is the most dramatic change to Japan's security strategy since adopting a pacifist constitution.",
+    urlToImage:
+      "https://ichef.bbci.co.uk/news/1024/branded_news/0E62/production/_128028630_081078748-2.jpg",
+  },
+  {
+    source: "https://www.bbc.com/news/world-asia-64001554",
+    title:
+      "Japan defence: China threat prompts plan to double military spending",
+    publishedAt: "BBC News",
+    description:
+      "It is the most dramatic change to Japan's security strategy since adopting a pacifist constitution.",
+    urlToImage:
+      "https://ichef.bbci.co.uk/news/1024/branded_news/0E62/production/_128028630_081078748-2.jpg",
+  },
+];
+
+export default initialNews;
