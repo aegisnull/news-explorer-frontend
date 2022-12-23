88bff86db52105d531eeefea696d7c05a3049f99
Luis Tellez <luis@luistellez.com>
2022-12-22 17:28:27 -0800

    feat: import React to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SavedNews from "./components/SavedNews/SavedNews";
 import NotFound from "./components/NotFound/NotFound";
 import { Routes, Route } from "react-router-dom";
 import SignInPopup from "./components/PopupWithForm/SignInPopup";
+import React from "react";
 
 function App() {
   return (
