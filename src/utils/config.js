84269a7872fef79f3497cc64813818a69dd4ac9c
Luis Tellez <luis@luistellez.com>
2022-12-22 21:01:18 -0800

    feat: add base config cof API

---
+++ src/utils/config.js
@@ -0,0 +1,15 @@
+const INITIAL_CARDS = 3;
+const PAGE_SIZE = 100;
+const SEARCH_INTERVAL = 7 * 24 * 3600 * 1000; // 7 days
+const API_KEY = "955ec7a803e045d8982c597a789f7166";
+const NEWS_URL = "https://newsapi.org/v2";
+const PROXY_URL = "https://nomoreparties.co/news/v2";
+
+export {
+  PAGE_SIZE,
+  INITIAL_CARDS,
+  SEARCH_INTERVAL,
+  API_KEY,
+  NEWS_URL,
+  PROXY_URL,
+};
