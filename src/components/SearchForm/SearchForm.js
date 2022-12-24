8acc17b27785625b5c08053bb2a50e646db82673
Luis Tellez <luis@luistellez.com>
2022-12-23 21:30:59 -0800

    feat: init NewsContext

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./SearchForm.scss";
+import NewsCard from "../NewsCard/NewsCard";
+
+/* function getNewsObject(searchInput) {
+  getNews(searchInput).then((articles) => {
+    return articles;
+  });
+} */
 
 function SearchForm(props) {
+  const [searchInput, setSearchInput] = React.useState("");
+
+  function handleSearchInput(e) {
+    setSearchInput(e.target.value);
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
     props.onSearch();
+    // getNewsObject(searchInput);
   }
+
+  <NewsCard formValue={searchInput} />;
 
   return (
     <form className="search-form" onSubmit={handleSubmit}>
@@ -13,6 +29,7 @@ function SearchForm(props) {
         type="text"
         className="search-form__input"
         placeholder="Introduce un tema"
+        onChange={handleSearchInput}
       />
       <button className="search-form__button">Buscar</button>
     </form>
