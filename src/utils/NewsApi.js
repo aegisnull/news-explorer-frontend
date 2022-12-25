bd1bf548a23bc8256ee109761fdd775f5be9d0f2
Luis Tellez <luis@luistellez.com>
2022-12-25 00:27:50 -0800

    refactor: refactor getNews to have the fetch request easily customizable

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -5,10 +5,13 @@ const API_KEY = "ed4390ffc54146c7a2ff5ea1673c8b01";
 
 // Function to get news
 function getNews(keyword) {
+  const sevenDaysAgo = new Date(
+    Date.now() - 7 * 24 * 3600 * 1000
+  ).toDateString();
+  const today = new Date().toDateString();
+
   return fetch(
-    `${PROXY_URL}/everything?q=${keyword}&from=${new Date(
-      Date.now() - 7 * 24 * 3600 * 1000
-    ).toISOString()}&to=${new Date().toISOString()}&pageSize=100&apiKey=${API_KEY}`,
+    `${PROXY_URL}/everything?q=${keyword}&from=${sevenDaysAgo}&to=${today}&pageSize=100&apiKey=${API_KEY}`,
     {
       method: "GET",
       headers: {
