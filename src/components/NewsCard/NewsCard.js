e1cfacd9cb4cb0fc9fe496b77d123d6ba7e399f0
Luis Tellez <luis@luistellez.com>
2022-12-25 00:30:44 -0800

    refactor: display News Card published date in structured spanish format

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -91,7 +91,13 @@ function Card(props) {
       >
         <img className="card__image" src={props.urlToImage} alt={props.title} />
         <div className="card__container">
-          <p className="card__date">{props.publishedAt}</p>
+          <p className="card__date">
+            {new Date(props.publishedAt).toLocaleDateString("es-MX", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}
+          </p>
           <h3 className="card__title">{props.title}</h3>
           <p className="card__text">{props.content}</p>
           <p className="card__publisher">{props.source}</p>
