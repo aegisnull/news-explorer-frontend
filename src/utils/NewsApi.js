c17ac05c434b2ff3429a442b0346c77ba15d8304
Luis Tellez <luis@luistellez.com>
2022-12-28 16:55:35 -0800

    refactor: modify NewsApi for main getNews function to run asynchrously

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -4,13 +4,13 @@ const PROXY_URL = "https://nomoreparties.co/news/v2";
 const API_KEY = "ed4390ffc54146c7a2ff5ea1673c8b01";
 
 // Function to get news
-function getNews(keyword) {
+async function getNews(keyword) {
   const sevenDaysAgo = new Date(
     Date.now() - 7 * 24 * 3600 * 1000
   ).toDateString();
   const today = new Date().toDateString();
 
-  return fetch(
+  return await fetch(
     `${PROXY_URL}/everything?q=${keyword}&from=${sevenDaysAgo}&to=${today}&pageSize=100&apiKey=${API_KEY}`,
     {
       method: "GET",
